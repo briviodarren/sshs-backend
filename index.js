4981862691a0d2701692fa30f06c01d8f69fa802
unknown <briviodarren@gmail.com>
2025-10-17 22:25:05 +0700

    fix: Finalize production CORS policy

--- index.js
+++ index.js
@@ -19,20 +19,7 @@ const allowedOrigins = [
   'http://localhost:5173'                                 // For local testing
 ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
-// -----------------------------
-
+app.use(cors());
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
